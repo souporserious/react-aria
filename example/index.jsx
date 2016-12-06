7293048fa92c8a59d9954f462dfa14e781e084c0
Travis Arnold <ftntravis@gmail.com>
2016-12-05 17:10:40 -0800

    update dev build

--- example/index.jsx
+++ example/index.jsx
@@ -1,10 +1,9 @@
 import React, { Component, PropTypes, Children, createElement } from 'react'
 import ReactDOM, { findDOMNode } from 'react-dom'
 import a11y from 'react-a11y'
+import Portal from 'react-travel'
 import Transition from 'react-motion-ui-pack'
 import { AriaManager, AriaToggle, AriaPopover, AriaItem, AriaTabList, AriaTab, AriaPanel } from '../src/react-aria'
-
-import Collapse from 'react-collapse'
 
 // a11y(React)
 
@@ -144,7 +143,7 @@ class Tabs extends Component {
       title: 'Samuel 👨🏿',
       panel: <div><h1>No man, I don't eat pork</h1><p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p><p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends.</p></div>
     }],
-    activeId: 'two'
+    activeId: 't2'
   }
 
   _handleChange = (activeId) => {
--- src/react-aria.js
+++ src/react-aria.js
@@ -5,3 +5,15 @@ export AriaItem from './AriaItem'
 export AriaTabList from './AriaTabList'
 export AriaTab from './AriaTab'
 export AriaPanel from './AriaPanel'
+
+// rename
+// export Manager from './Manager'
+// export Toggle from './Toggle'
+// export Popover from './Popover'
+// export Item from './Item'
+// export TabList from './TabList'
+// export Tab from './Tab'
+// export Panel from './Panel'
+
+// usage
+// <Aria.Manager/> || <Manager/>
