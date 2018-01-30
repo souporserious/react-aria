c56f5da9da30f4367fb4d4991f0eaecf9dcb9a6a
Jake <krazyjakee@gmail.com>
2018-01-30 14:58:41 +0100

    Updated proptypes to be compatible with React >= 15.6.0

--- src/Items/ItemList.jsx
+++ src/Items/ItemList.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes, createElement, cloneElement } from 'react'
+import React, { Component, createElement, cloneElement } from 'react'
+import PropTypes from 'prop-types'
 import ReactDOM, { findDOMNode } from 'react-dom'
 import FocusGroup from '../utils/FocusGroup'
 import specialAssign from '../utils/special-assign'
